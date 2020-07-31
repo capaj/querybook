57598c542da0a54eb12ac9fef88f284af6720802
Jiri Spac <capajj@gmail.com>
2020-08-01 00:41:05 +0200

    type acquisition from existing schema implemented

--- frontend/parametrizeQuery.spec.ts
+++ frontend/parametrizeQuery.spec.ts
@@ -1,10 +1,15 @@
 import parametrizeQuery from './parametrizeQuery'
+import { parse } from 'graphql'
 
 describe('queryParametrizer', function () {
   it('should parametrize queries', async () => {
     expect(
       parametrizeQuery(
-        `{ foo (first: 213) { ok koo } foo2(second: "aaa") {id} foo3(dupe: {a: "aaa"}) {id} foo4(dupe: {b: "aaa"}) {id} }`
+        `{
+        foo (first: 213) { ok koo }
+        foo2(second: "aaa") {id}
+        foo3(dupe: {a: "aaa"}) {id}
+        foo4(dupe: {b: "aaa"}) {id} }`
       )
     ).toMatchInlineSnapshot(`
       "query ($first: Int, $second: String, $dupe1: Object, $dupe2: Object) {
@@ -40,4 +45,56 @@ describe('queryParametrizer', function () {
   //   it('should detect name collisions and resolve them by prepending a field name', async () => {
   //     expect(parametrizeQuery(`{ foo (first: 213) { ok koo } }`))
   //   })
+
+  it('should acquire types from the schema', () => {
+    expect(
+      parametrizeQuery(
+        `{
+        foo (first: 213) {
+          foo2(second: {a: "aaa"}) {
+            foo3(third: {b: "bbb"}) {
+              id
+            }
+          }
+         }
+        }`,
+        parse(`
+        input InputOne {
+          a: String
+        }
+
+        input InputSecond {
+          b: String
+        }
+        
+
+        type FooReturn {
+          foo2(second: InputOne): FooReturn2
+        }
+
+        type FooReturn2 {
+          foo3(third: InputSecond!): FooReturn3
+        }
+
+        type FooReturn3 {
+          id: Int!
+        }
+
+        type Query {
+          foo(first: Int!): FooReturn
+        }`)
+      )
+    ).toMatchInlineSnapshot(`
+      "query ($first: Int!, $second: InputOne, $third: InputSecond!) {
+        foo(first: $first) {
+          foo2(second: $second) {
+            foo3(third: $third) {
+              id
+            }
+          }
+        }
+      }
+      "
+    `)
+  })
 })
--- frontend/parametrizeQuery.ts
+++ frontend/parametrizeQuery.ts
@@ -1,13 +1,44 @@
 import { print } from 'graphql/language/printer'
-import { parse, visit } from 'graphql'
+import {
+  parse,
+  visit,
+  DocumentNode,
+  OperationDefinitionNode,
+  ObjectTypeDefinitionNode,
+} from 'graphql'
 import { findDuplicates } from './findDuplicates'
 
-// const findArgumentType = (source) => {}
+const capitalize = (s: string) => {
+  return s.charAt(0).toUpperCase() + s.slice(1)
+}
 
-export default function parametrizeQuery(query: string): string {
+export default function parametrizeQuery(
+  query: string,
+  schema?: DocumentNode
+): string {
   const parsed = parse(query)
 
   let allArgumentNames = []
+  const argumentsToTypesMap = new Map()
+  const fieldsMap = new Map()
+
+  if (schema) {
+    visit(schema, {
+      FieldDefinition(node, key, parent, path, ancestors) {
+        const lastAncestor = ancestors[ancestors.length - 1] as
+          | OperationDefinitionNode
+          | ObjectTypeDefinitionNode
+
+        for (const arg of node.arguments) {
+          const argGlobalKey = `${lastAncestor.name?.value}.${node.name.value}.${arg.name.value}`
+
+          argumentsToTypesMap.set(argGlobalKey, arg)
+        }
+
+        fieldsMap.set(`${lastAncestor.name?.value}.${node.name.value}`, node)
+      },
+    })
+  }
 
   visit(parsed, {
     Argument(node, key, parent, path, ancestors) {
@@ -17,13 +48,19 @@ export default function parametrizeQuery(query: string): string {
   })
 
   const duplicatedArgumentNames = findDuplicates(allArgumentNames)
-
+  let operationType
   const edited = visit(parsed, {
     OperationDefinition(node) {
+      operationType = capitalize(node.operation)
       const replacedLiterals = []
 
       visit(node, {
         Argument(node, key, parent, path, ancestors) {
+          const fieldPathFromRoot = ancestors
+            .filter((ancestor) => ancestor['kind'] === 'Field')
+            // @ts-ignore
+            .map((a) => a.name.value)
+
           let argumentName = node.name.value
 
           const alreadyNumberedArguments = replacedLiterals.filter(
@@ -38,13 +75,17 @@ export default function parametrizeQuery(query: string): string {
           ) {
             replacedLiterals.push({
               name: argumentName,
+              originalArgName: node.name.value,
               kind: node.value.kind,
+              fieldPathFromRoot,
             })
           } else {
             argumentName = argumentName + (alreadyNumberedArguments.length + 1)
             replacedLiterals.push({
               name: argumentName,
+              originalArgName: node.name.value,
               kind: node.value.kind,
+              fieldPathFromRoot,
             })
           }
 
@@ -60,8 +101,32 @@ export default function parametrizeQuery(query: string): string {
           return node
         },
       })
+
       // @ts-ignore
       node.variableDefinitions = replacedLiterals.map((replaced) => {
+        let type = {
+          kind: 'NamedType',
+          name: {
+            kind: 'Name',
+            value: replaced.kind.replace('Value', ''), // todo try to get a type from schema
+          },
+        }
+        if (schema) {
+          let objectTypeName = operationType
+          let inputValueDefinition
+
+          for (const fieldName of replaced.fieldPathFromRoot) {
+            const fieldMapKey = `${objectTypeName}.${fieldName}`
+            const argMapKey = `${fieldMapKey}.${replaced.originalArgName}`
+
+            inputValueDefinition = argumentsToTypesMap.get(argMapKey)
+            if (inputValueDefinition) {
+              type = inputValueDefinition.type
+            } else {
+              objectTypeName = fieldsMap.get(fieldMapKey).type.name.value
+            }
+          }
+        }
         return {
           kind: 'VariableDefinition',
           variable: {
@@ -71,13 +136,7 @@ export default function parametrizeQuery(query: string): string {
               value: replaced.name,
             },
           },
-          type: {
-            kind: 'NamedType',
-            name: {
-              kind: 'Name',
-              value: replaced.kind.replace('Value', ''),
-            },
-          },
+          type,
         }
       })
       return node
